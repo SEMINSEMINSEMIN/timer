58482dea3e00bb4c8c75ce00aeda6eb8d774b09e
SeminKang <ksm9802@gmail.com>
2022-11-09 07:56:12 +0900

    클래스로 데이터 관리

--- index.js
+++ index.js
@@ -1,51 +1,79 @@
-const $form = document.querySelector('.timer-settings');
-const $startBtn = $form.querySelector('.start-btn');
-$form.addEventListener('submit', e => e.preventDefault());
-
 class Timer {
-    constructor($hrs, $min, $sec){
-        this.leftTime = $sec + $min * 60 + $hrs * 3600;
+    constructor(){
+        const $form = document.querySelector('.timer-settings');
+        $form.addEventListener('submit', e => e.preventDefault());
+        this.$hrs = $form.querySelector('#hrs');
+        this.$min = $form.querySelector('#min');
+        this.$sec = $form.querySelector('#sec');
+        this.$startBtn = $form.querySelector('.start-btn');
+        this.$pauseBtn = $form.querySelector('.pause-btn');
+        this.leftTime = 0;
+        this.isPaused = false;
+    }
+
+    setup(){
+        this.bindEvents();
     }
 
     timerStart(){
-        if (this.leftTime <= 0){
-            console.log('끝');
-            return
-        }
-        setTimeout(() => {
-            this.leftTime = this.leftTime - 1;
-            console.log(this.leftTime);
+        console.log(this.$hrs.value);
+        console.log(this.$min.value);
+        console.log(this.$sec.value);
+        this.leftTime = 
+            parseInt(this.$sec.value) + 
+            parseInt(this.$min.value * 60) + 
+            parseInt(this.$hrs.value * 3600);
+        // if (this.leftTime <= 0 || this.isPaused == true){
+        //     console.log('끝');
+        //     return
+        // }
+        // setTimeout(() => {
+        //     this.leftTime = this.leftTime - 1;
+        //     console.log(this.leftTime);
+        //     this.timerStart();
+        // }, 1000)
+    }
+
+    bindEvents(){
+        this.$startBtn.addEventListener('click', e => {
             this.timerStart();
-        }, 1000)
+        });
     }
 }
 
-function validation(hrsEl, minEl, secEl){
-    if (
-        (hrsEl.value >= hrsEl.min && hrsEl.value <= hrsEl.max) &&
-        (minEl.value >= minEl.min && minEl.value <= minEl.max) &&
-        (secEl.value >= secEl.min && secEl.value <= secEl.max)
-    ) {
-        return true;
-    } else {
-        return false;
-    }
-}
+const timer = new Timer();
+timer.setup();
 
-$startBtn.addEventListener('click', e => {
-    const $hrs = $form.querySelector("#hrs");
-    const $min = $form.querySelector("#min");
-    const $sec = $form.querySelector("#sec");
+// function validation(hrsEl, minEl, secEl){
+//     if (
+//         (hrsEl.value >= hrsEl.min && hrsEl.value <= hrsEl.max) &&
+//         (minEl.value >= minEl.min && minEl.value <= minEl.max) &&
+//         (secEl.value >= secEl.min && secEl.value <= secEl.max)
+//     ) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
 
-    if (validation($hrs, $min, $sec)){
-        const timeData = new Timer(
-            parseInt($hrs.value), 
-            parseInt($min.value), 
-            parseInt($sec.value)
-        );
-        console.log(timeData);
-        timeData.timerStart();
-    } else {
-        window.alert("값을 제대로 입력하세요.");
-    }
-});
+// $startBtn.addEventListener('click', e => {
+//     const $hrs = $form.querySelector("#hrs");
+//     const $min = $form.querySelector("#min");
+//     const $sec = $form.querySelector("#sec");
+
+//     if (validation($hrs, $min, $sec)){
+//         const timeData = new Timer(
+//             parseInt($hrs.value), 
+//             parseInt($min.value), 
+//             parseInt($sec.value)
+//         );
+//         console.log(timeData);
+//         timeData.timerStart();
+//     } else {
+//         window.alert("값을 제대로 입력하세요.");
+//     }
+// });
+
+// $pauseBtn.addEventListener('click', e => {
+
+// })
